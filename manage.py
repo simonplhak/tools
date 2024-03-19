dc779275817ff9557f36966648699f99a9d90e0c
Simon Plhak <plhak.s@gmail.com>
2024-03-19 08:24:10 +0100

    feat: image-properties command

--- manage.py
+++ manage.py
@@ -4,22 +4,24 @@ import os
 import click
 from PIL import Image
 
+
 @click.group()
 def cli():
     pass
+
 
 @cli.command()
 @click.argument('image_path', type=click.Path(exists=True))
 @click.option('--width', default=100, help='Width of the resized image.')
 @click.option('--height', default=100, help='Height of the resized image.')
 @click.option('--output', default='./', help='Path where the resized image will be stored.')
-def resize_image(image_path : str, width: int, height: int, output: str):
+def resize_image(image_path: str, width: int, height: int, output: str):
     """
     Resize an image maintaining its aspect ratio.
     """
     original_image = Image.open(image_path)
     resized_img = original_image.resize((width, height))
-    
+
     # create folder if doesn't exist
     if not os.path.exists(output):
         os.makedirs(output)
@@ -33,6 +35,19 @@ def resize_image(image_path : str, width: int, height: int, output: str):
     resized_img.save(output_path)
     click.echo(f'Resized image saved at {output_path}')
 
+
+@cli.command()
+@click.argument('image_path', type=click.Path(exists=True))
+def image_properties(image_path: str):
+    """
+    Get the properties of an image.
+    """
+    img = Image.open(image_path)
+    click.echo(f'Image format: {img.format}')
+    click.echo(f'Image mode: {img.mode}')
+    click.echo(f'Image size: {img.size}')
+
+
 # Here is an example of an additional command
 @cli.command()
 def hello():
@@ -40,6 +55,7 @@ def hello():
     Just a command that prints Hello World!
     """
     click.echo('Hello World!')
+
 
 if __name__ == "__main__":
     cli()
